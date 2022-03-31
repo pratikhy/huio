5a7962e85f1ea22d26478846f37ccc3505a1f574
pratikhy <95514911+pratikhy@users.noreply.github.com>
2022-03-31 19:28:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const { fabric } = require("./fabric");
+
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromUrl('BirthdayImage.jpg', function(img){(
+        block-image-object = Img;
+
+        block-image-object.scaleToWidth(700);
+        block-image-object.scaleToHeight(560);
+        block-image-object.set({
+        top:0,
+        left:0
+        });
+        canvas.(block-image-object);
+    });
+    
+    
+}
+
+function playSound(){
+	x.play();
+}
